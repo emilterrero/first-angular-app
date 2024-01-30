fb7f607229cd49129b3f71af330c3b035c3b3e72
Emil Terrero <emil@Emils-MacBook-Pro.local>
2024-01-30 14:05:17 -0500

    Input and Search Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: '<h1>Hello World</h1>',
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by City">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
